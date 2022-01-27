0ebfaa08d081bdda1435ca465b54a5c0005d5f2b
Rajani Kant Raghav <82476053+mrraghav9211@users.noreply.github.com>
2022-01-27 13:23:04 +0530

    Jan_26_assignment1

---
+++ Jan_26_assignment/hello.js
@@ -0,0 +1,11 @@
+var char = prompt("enter any character");
+
+if(char == 'a' || char == 'A' || char == 'e'|| char == 'E' || char =='i' || char =='I' || char == 'o' || char
+=='O' ){
+	alert("charecter is VOVEL");
+}
+else
+{
+	alert("character  is CONSONANT");
+
+}
