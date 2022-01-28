238e5fcd4ab6aa7474fb901a38b4415a0c21fcd1
Rajani Kant Raghav <82476053+mrraghav9211@users.noreply.github.com>
2022-01-28 10:46:57 +0530

    Update index.js

--- Jan_27_assignment/index.js
+++ Jan_27_assignment/index.js
@@ -1,19 +1,4 @@
-// var noOfNum=prompt(" enter the total number");
-// var arr=[];
-// var i=0;
-// for(i=0;i<noOfNum;i++){
-//     var value=prompt("please enter value");
-//     arr.push(value);
 
-// }
-// var max = arr[0];
-// for(i=0;i<noOfNum;i++){
-//     if (arr[i]>max) {
-//         max = arr[i];
-
-//     }
-// }
-// console.log(max);
 var totalVal = prompt("enter toatal value");
 var arr = [];
 var i = 0;
